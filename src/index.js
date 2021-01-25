65192de881a97b2f373a261ace7ec2bd2bd3e851
Ritik <nandanwarritik10@gmail.com>
2021-01-25 20:44:53 +0530

    Modified some files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
-import './materialize.css'
-
-
+import StripeApp from './StripeApp'
 
 ReactDOM.render(
-
   <>
+    {/* <StripeApp /> */}
     <App />
   </>,
   document.getElementById('root')
